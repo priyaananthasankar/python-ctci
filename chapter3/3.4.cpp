8b532382cd348ccf9d5d1bde328a1ba38f51858b
Priya Ananthasankar <panantha@Priyas-MacBook-Pro.local>
2016-10-03 23:58:21 -0700

    towers of hanoi

---
+++ chapter3/3.4.cpp
@@ -0,0 +1,27 @@
+// Author : Priya Ananthasankar
+// Goal: Write a program to simulate Tower of Hanoi problem
+// Commands: g++ 3.2.cpp; ./a.out
+#include<iostream>
+using namespace std;
+
+void hanoi(int n, int source, int dest, int spare){
+	if(n == 0){
+		cout<< "Move disk " << n << " from source " << source << " to destination " << dest << endl;
+	}else{
+		hanoi(n-1,source,spare,dest);
+		cout<< "Move disk " << n << " from source " << source << " to destination " << dest << endl;
+		hanoi(n-1,spare,dest,source);
+	}
+}
+
+
+int main(){
+
+	int n;
+	
+	cout << "Enter the number of disks: " << endl;
+	cin >> n;
+
+	hanoi(n,0,1,2);
+
+}
