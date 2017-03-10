0af15795f5d8a929bb40a647e5e4f451f46b1fe9
Priya Ananthasankar <panantha@Priyas-MacBook-Pro.local>
2017-03-09 21:48:53 -0800

    delete key from array

---
+++ misc/DeleteKey.java
@@ -0,0 +1,20 @@
+public class DeleteKey {
+
+  public static void main(String args[]){
+     int a[] = {5,3,7,11,2,3,13,5,7};
+     int k = 3;
+     int j = 0;int i=0;
+     int n = a.length;
+     while (j < a.length){
+       if(a[j] != k){
+         a[i] = a[j];
+         i++;
+       }
+       j++;
+     }
+     for(int p =0; p<i;p++){
+     System.out.print(a[p] + " ");
+    }
+  }
+
+}
